8114a69f0cd4f4590d2ca17d0f13db1bc2a7a249
femvoeduc <ssgomes775@gmail.com>
2026-06-21 21:45:16 -0400

    Update sheets-loader.js

--- sheets-loader.js
+++ sheets-loader.js
@@ -8,7 +8,7 @@
 // https://docs.google.com/spreadsheets/d/ESTE_ES_EL_ID/edit
 // ============================================================
 
-const SHEET_ID = "17o0lNkUjbkWUZrsRujEhkd6WZwqeyahG";
+const SHEET_ID = "167QKtJf2E4HvMGhVjLSvsOPfv3weEKVk";
 
 // No tocar: construye la URL de lectura para cada pestaña
 function sheetUrl(tabName){
